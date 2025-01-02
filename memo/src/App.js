0e72bd7705d4bbd65a38d9693b2e40c6e0c8b9ee
주자훈 <jahoon@Jahoon-MacBookPro.local>
2025-01-02 22:06:55 +0900

    add:react-memo-app

---
+++ memo/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import MemoContainer from './components/MemoContainer';
+
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
---
+++ memo/src/components/MemoContainer.js
@@ -0,0 +1,11 @@
+function MemoContainer(){
+    return (
+        <div>
+            <div>
+                <input type="text" placeholder="Title">안녕하세요</input>
+            </div>
+        </div>
+    )
+}
+
+export default MemoContainer;
