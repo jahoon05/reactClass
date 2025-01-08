14d34c28e6e6381d6c4005885382797206e5c380
주자훈 <jahoon@Jahoon-MacBookPro.local>
2025-01-08 23:06:23 +0900

    fix:components

--- memo/src/App.js
+++ memo/src/App.js
@@ -5,8 +5,11 @@ import MemoContainer from './components/MemoContainer';
 function App() {
   return (
     <div className="App">
+      <header className="App-header">
+        <MemoContainer />
+      </header>
     </div>
-  );
+  )
 }
 
 export default App;
